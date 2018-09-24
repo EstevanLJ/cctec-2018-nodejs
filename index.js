62579db425a44459173d6aefcae10502ce911af0
EstevanLJ <estevanjunges@gmail.com>
2018-09-24 20:18:33 -0300

    Ajustes porta

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require('dotenv').config()
+const PORT = process.env.PORT || 5000
+
 const express = require('express')
 const sqlite3 = require('sqlite3')
 const bodyParser = require('body-parser')
 
 const app = express()
 const db = new sqlite3.Database('database.sqlite')
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -16,6 +20,6 @@ require('./controllers/produtos')(app, db)
 require('./controllers/vendas')(app, db)
 require('./controllers/sistema')(app)
  
-app.listen(3000, () => {
-    console.log('Up on port http://localhost:3000');
+app.listen(PORT, () => {
+    console.log('Up on port ' + PORT);
 })

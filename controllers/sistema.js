7cd44582e43ff33cfc23ae0eec72609e7b185d30
EstevanLJ <estevanjunges@gmail.com>
2018-10-04 13:33:56 -0300

    Adicionada rota para baixar o modelo do banco

--- controllers/sistema.js
+++ controllers/sistema.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 module.exports = (app, db) => {
 
     app.get('/reiniciar', (req, res) => {
-
         fs.copyFile('./database.sqlite.example', './database.sqlite', (err) => {
             if (err) {
                 console.log(err)
@@ -12,8 +11,11 @@ module.exports = (app, db) => {
                 res.send()
             }
         })
+    })
 
-
+    app.get('/download', (req, res) => {
+        var file = './database.sqlite.example';
+        res.download(file); 
     })
 
 }
